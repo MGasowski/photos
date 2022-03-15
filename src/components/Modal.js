ed8dd71add00b3a6bc7168f2e1429f7985519d8e
s15441 <s15441@pjwstk.edu.pl>
2022-03-16 00:54:12 +0100

    Added: - Gallery component with images fetched from api - Image component with modal on click - Pagination section - Dark theme switch

---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+const Modal = (props) => {
+  return (
+    <div class=" overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center h-modal md:h-full md:inset-0 bg-cyan-300">
+      sss
+    </div>
+  );
+};
+
+export default Modal;
